f0b5f6bfa6a14b29b9cec1953608193c4f6a1aba
Lance Kelley <lancekkcodes@gmail.com>
2021-12-19 16:59:28 -0600

    fix cache behavior, cache idb.js, recache index.html

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,19 +2,21 @@ const APP_PREFIX = 'BudgetTracker-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX+VERSION;
 const FILES_TO_CACHE = [
+  "./index.html",
   "./js/index.js",
+  "./js/idb.js",
   "./css/styles.css",
 ];
 
 self.addEventListener('fetch', function (e) {
-  console.log('fetch request: ' + e.request_url);
+  console.log('fetch request: ' + e.request.url);
   e.respondWith(
-    caches.match(e.request).then(function(request) {
-      if (request) {
-        console.log('responding with cache: ' + e.request_url);
-        return request;
+    caches.match(e.request).then(function(cached) {
+      if (cached) {
+        console.log('responding with cache: ' + e.request.url);
+        return cached;
       } else {
-        console.log('file is not cached, fetching: ' + e.request_url);
+        console.log('file is not cached, fetching: ' + e.request.url);
         return fetch(e.request);
       }
     })
